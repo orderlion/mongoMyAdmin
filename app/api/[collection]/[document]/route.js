22c7adcf1911a6f06147be5cef187ce7b40a5a27
Patrick Schubert <patrick.schubert1@gmail.com>
2023-06-15 12:15:43 +0200

    delete single documents

--- app/api/[collection]/[document]/route.js
+++ app/api/[collection]/[document]/route.js
@@ -28,6 +28,24 @@ export async function POST(req, { params = {} }) {
   if (idBytes === 12 || idBytes === 24) {
     newDocId = new ObjectId(newDocId);
   }
+  if (Object.keys(newDoc).length === 0) {
+    // apparently we removed everything except the id
+    await collection.deleteOne({ _id: newDocId });
+    const insertResp = await collection.insertOne({ _id: newDocId });
+    return NextResponse.json(insertResp);
+  }
   const updateResp = await collection.updateOne({ _id: newDocId }, { $set: { ...newDoc } }, { upsert: true });
   return NextResponse.json(updateResp);
+}
+
+export async function DELETE(req, { params = {} }) {
+  let { collection: collectionName, document: _id } = params;
+  const collection = await getCollection(collectionName);
+  if (!collection) throw new Error(`Collection ${collectionName} not found.`);
+  const idBytes = Buffer.from(_id).length;
+  if (idBytes === 12 || idBytes === 24) {
+    _id = new ObjectId(_id);
+  }
+  const deleteResp = await collection.deleteOne({ _id });
+  return NextResponse.json(deleteResp);
 }
--- app/collection/[collection]/[document]/page.js
+++ app/collection/[collection]/[document]/page.js
@@ -4,7 +4,8 @@ import React, { useState, useEffect } from 'react';
 import useSWR from 'swr';
 import Editor from '@monaco-editor/react';
 import { useRouter } from 'next/navigation';
-import { Title, Text, ThemeIcon, Loader, Button, Flex } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { Title, Text, ThemeIcon, Loader, Button, Flex, Modal } from '@mantine/core';
 import { jsonrepair } from 'jsonrepair';
 import useNotifications from '@/src/ui/useNotifications';
 
@@ -18,6 +19,8 @@ export default function Document({ params }) {
   const { collection, document: _id } = params;
   const router = useRouter();
   const notifications = useNotifications();
+
+  const [confirmModalOpened, { open: openConfirmModal, close: closeConfirmModal }] = useDisclosure(false);
 
   const { data: document, error, isLoading } = useSWR(
     `${collection}-${_id}`,
@@ -43,25 +46,34 @@ export default function Document({ params }) {
     if (e) e.preventDefault();
     try {
       const newDoc = JSON.parse(jsonrepair(documentStr));
-      sendNewDocument(newDoc);      
+      saveNewDocument(newDoc);      
     } catch(err) {
       console.error(err);
       notifications.error(`Something went wrong! Is your JSON correct? Check the console!`);
     }
   }
 
-  async function sendNewDocument(newDoc) {
+  function deleteDocument(e) {
+    if (e) e.preventDefault();
+    console.log('DELETE', _id);
+    saveNewDocument(null, 'DELETE');
+  }
+
+  async function saveNewDocument(newDoc, method = 'POST') {
     try {
       const resp = await fetch(`/api/${collection}/${_id}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
+        method,
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(newDoc)
       });
       const updateResp = await resp.json();
-      console.log('POST new document - Resp:', updateResp);
-      notifications.success(`Document saved successfully!`);
+      if (method === 'DELETE') {
+        closeConfirmModal();
+        notifications.success(`Document deleted successfully!`);
+        router.back();
+      } else {
+        notifications.success(`Document saved successfully!`);
+      }
     } catch(err) {
       console.error(err);
       notifications.error(`Something went wrong! Check the console!`);
@@ -70,10 +82,19 @@ export default function Document({ params }) {
 
   return (
     <>
-      <Title order={2}>{_id}</Title>
-      <Text mb="xl">
-        <a onClick={() => router.back()}><b>&laquo; BACK</b></a> | <a onClick={() => copyToClipboard(_id)}>Copy _id</a> | <a onClick={() => copyToClipboard(location.href)}>Copy link</a>
-      </Text>
+      <Flex mb="lg" align="center" justify="space-between">
+        <div>
+          <Title order={2}>{_id}</Title>
+          <Text mb="xl">
+            <a onClick={() => router.back()}><b>&laquo; BACK</b></a> | <a onClick={() => copyToClipboard(_id)}>Copy _id</a> | <a onClick={() => copyToClipboard(location.href)}>Copy link</a>
+          </Text>
+        </div>
+        <div>
+          <Button size="sm" ml="sm" color="red" onClick={openConfirmModal}>
+            Delete document
+          </Button>
+        </div>
+      </Flex>
       { isLoading && <Loader color="green" size="sm" style={{ margin: 'auto' }} /> }
       { error && <pre>{error}</pre> }
       { !isLoading && !error && (
@@ -93,6 +114,17 @@ export default function Document({ params }) {
           </Flex> 
         </>
       )}
+      <Modal opened={confirmModalOpened} onClose={closeConfirmModal} title="Are you sure?" centered>
+        <Text size="md">Do you want to delete this document?</Text>
+        <Flex mt="lg" align="center" justify="space-between">
+          <Button size="sm" color="gray" onClick={closeConfirmModal}>
+            Cancel
+          </Button>
+          <Button size="sm" color="red" onClick={deleteDocument}>
+            DELETE
+          </Button>
+        </Flex>
+      </Modal>
     </>
   )
 }
