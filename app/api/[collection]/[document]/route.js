a21d637abdf5be87d94ddaf36f9e687fae823e17
Patrick Schubert <patrick.schubert1@gmail.com>
2023-07-10 21:29:35 +0200

    bugfix that Dates do not become Strings after saving :)

--- app/api/[collection]/[document]/route.js
+++ app/api/[collection]/[document]/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import { getCollection } from '@/src/services/mongodb';
+import objectPath from 'object-path';
 import { ObjectId } from 'mongodb'; 
 
 // DOCUMENT LEVEL
@@ -21,6 +22,13 @@ export async function POST(req, { params = {} }) {
   const { collection: collectionName, document: _id } = params;
   const collection = await getCollection(collectionName);
   if (!collection) throw new Error(`Collection ${collectionName} not found.`);
+  const oldIdBytes = Buffer.from(_id).length;
+  let findById = { _id };
+  // search for _id directly (e.g. for Meteor, where _ids are strings) as well as its ObjectId:
+  if (oldIdBytes === 12 || oldIdBytes === 24) {
+    findById = { $or: [{ _id }, { _id: new ObjectId(_id) }] };
+  }
+  const oldDoc = await collection.findOne(findById);
   const newDoc = await req.json();
   let newDocId = newDoc._id;
   delete newDoc._id;
@@ -33,6 +41,10 @@ export async function POST(req, { params = {} }) {
     await collection.deleteOne({ _id: newDocId });
     const insertResp = await collection.insertOne({ _id: newDocId });
     return NextResponse.json(insertResp);
+  }
+  // iterate over newDoc and check, that all the data types are correct - especially dates!
+  if (oldDoc && newDoc) {
+    ensureAttrType({ value: newDoc, compare: oldDoc });
   }
   const updateResp = await collection.updateOne({ _id: newDocId }, { $set: { ...newDoc } }, { upsert: true });
   return NextResponse.json(updateResp);
@@ -48,4 +60,39 @@ export async function DELETE(req, { params = {} }) {
   }
   const deleteResp = await collection.deleteOne({ _id });
   return NextResponse.json(deleteResp);
+}
+
+// could be used later for setting "new" values of type date:
+const dateRegex = /^(\d{4})-0?(\d+)-0?(\d+)[T ]0?(\d+):0?(\d+):0?(\d+)(\.(\d+)Z)?$/g;
+
+// this helper function makes sure, that the TYPE of a value persists!
+function ensureAttrType({ value, compare, orig = null, path = '' }) {
+  if (path === '' && !orig) orig = value;
+  if (Array.isArray(value) && value?.length) {
+    value.forEach((arrayElem, arrayIndex) => {
+      ensureAttrType({ value: arrayElem, compare, orig, path: `${path}.${arrayIndex}` });
+    });
+    return;
+  }
+  if (typeof value === 'object' && value && !Array.isArray(value)) {
+    for (const [key, innerValue] of Object.entries(value)) {
+      const innerPath = [path, key].filter((v) => !!v).join('.');
+      ensureAttrType({ value: innerValue, compare, orig, path: innerPath });
+    }
+    return;
+  }
+  const compareValue = objectPath.get(compare, path);
+  // not a new value, but an updated one, but the TYPE changed:
+  if (typeof compareValue !== 'undefined' && typeof value !== typeof compareValue) {
+    if (compareValue instanceof Date) {
+      value = new Date(value);
+    }
+    if (typeof compareValue === 'number') {
+      value = parseFloat(value);
+    }
+    if (typeof compareValue === 'string') {
+      value = String(value);
+    }
+    objectPath.set(orig, path, value);
+  }
 }
